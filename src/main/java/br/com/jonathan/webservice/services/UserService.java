6f1fddd2bdc475020ab69c4339e1d08bfa060485
jonathan abreu <abreu.jonathan220@gmail.com>
2024-04-30 20:57:56 -0300

    Exception handling - findById

--- src/main/java/br/com/jonathan/webservice/services/UserService.java
+++ src/main/java/br/com/jonathan/webservice/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.jonathan.webservice.services;
 
 import br.com.jonathan.webservice.entities.User;
 import br.com.jonathan.webservice.repositories.UserRepository;
+import br.com.jonathan.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
---
+++ src/main/java/br/com/jonathan/webservice/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.jonathan.webservice.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
