05fc06de25133a38ff6e990d5bffe16d1e91d2c2
jonathan abreu <abreu.jonathan220@gmail.com>
2024-04-29 23:35:46 -0300

    User insert

--- src/main/java/br/com/jonathan/webservice/resources/UserResource.java
+++ src/main/java/br/com/jonathan/webservice/resources/UserResource.java
@@ -5,10 +5,7 @@ import br.com.jonathan.webservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,11 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        User resultado = userService.insert(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
     }
 }
