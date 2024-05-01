73f2ade8c3415a3f01f898eb3d468ec757e9a753
jonathan abreu <abreu.jonathan220@gmail.com>
2024-04-30 22:30:06 -0300

    Exception handling - delete

--- src/main/java/br/com/jonathan/webservice/services/UserService.java
+++ src/main/java/br/com/jonathan/webservice/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.jonathan.webservice.services;
 
 import br.com.jonathan.webservice.entities.User;
 import br.com.jonathan.webservice.repositories.UserRepository;
+import br.com.jonathan.webservice.services.exceptions.DatabaseException;
 import br.com.jonathan.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User user){
